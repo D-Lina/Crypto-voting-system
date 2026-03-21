45d6318bc19289755cc55254c1673276ebca444f
D.Leena <al.djediai@ensta.edu.dz>
2026-03-21 17:21:01 +0100

    Update blind_signature.py

--- core/crypto/blind_signature.py
+++ core/crypto/blind_signature.py
@@ -1,1 +1,27 @@
+import random
 
+def generate_blinding_factor(n):
+    return random.randint(2, n - 1)
+
+
+def blind_message(message, public_key):
+    e, n = public_key
+    
+    r = generate_blinding_factor(n)
+    blinded = (message * pow(r, e, n)) % n
+    
+    return blinded, r
+
+
+def blind_sign(blinded_message, private_key):
+    d, n = private_key
+    return pow(blinded_message, d, n)
+
+
+def unblind_signature(blind_signature, r, public_key):
+    n = public_key[1]
+    
+    r_inv = pow(r, -1, n)
+    signature = (blind_signature * r_inv) % n
+    
+    return signature
