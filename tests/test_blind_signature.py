68091b10b7b4ad0796a1b9e2754662f67709670b
D.Leena <al.djediai@ensta.edu.dz>
2026-03-24 13:30:23 +0100

    Update test_blind_signature.py
    Purpose : Tests the full blind signature workflow
    Main Functions :
    * test_blind_signature() : Runs full process (blind → sign → unblind → verify)
    * verify(signature, message, public_key, hash_func) : Verifies signature using hashed message
    Workflow :
    1.Input message
    2.Blind message
    3.Sign blinded message
    4.Unblind signature
    5.Verify result
    6.Display stats
    Dependencies :
    * core.crypto.blind_signature
    * core.utils.blind_utils
    * core.utils.counters
    Important Notes !!
    Uses temporary hash function
    * Designed for testing and demonstration
    * Uses fake RSA keys
    * Will integrate with real RSA keys later ( @ilham2006-hup )
    
     sorry @celina331 for interrupting your work :')

--- tests/test_blind_signature.py
+++ tests/test_blind_signature.py
@@ -8,18 +8,23 @@ from core.utils.counters import get_stats
 
 public_key = (17, 3233)
 private_key = (2753, 3233)
+
 def verify(signature, message, public_key, hash_func):
     e, n = public_key
     hashed = hash_func(message)
     return pow(signature, e, n) == hashed
 def test_blind_signature():
     message = 7
-
     print("Original message:", message)
+
     try:
+        # Step 1: blind
         blinded, r = blind_message(message, public_key, hash_message)
+        # Step 2: sign
         signed_blind = blind_sign(blinded, private_key)
+        # Step 3: unblind
         signature = unblind_signature(signed_blind, r, public_key)
+        # Step 4: verify
         if verify(signature, message, public_key, hash_message):
             print("\n✅ SUCCESS: Blind signature works!")
         else:
